1af09c189654f095e7eec92b56cf8bd5612d8c9f
Brandon Vu <bvu106038@gmail.com>
2020-06-25 23:16:25 +0000

    add createMap() and bothFunc() to call two functions in HTML

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -40,12 +40,107 @@ function addMeme()
     }
 }
 
-//Parsing JSON comments
+/**Create Map  with styling and location */
+function createMap() {
+var location = {lat: 33.870350, lng: -117.924301};
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: location, zoom: 10,
+       styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+      });
+      var marker = new google.maps.Marker({position: location, map: map});
+}
+/** Call both functions when the website first loads */
+function bothFunc()
+{
+    createMap();
+    getComments();
+}
+
+/**Parsing JSON comments*/
 function getComments()
 {
     var bruh = document.getElementById('nComment').value;
     var ending = "/data?nComment=" + bruh;
-    console.log(ending);
     fetch(ending).then(response => response.json()).then((commentObj) => {
     var allComments = document.getElementById('allComments');
     while (allComments.hasChildNodes())
@@ -74,4 +169,5 @@ function deleteAll()
 {
     fetch('/delete-data', {method: 'POST'}).then(getComments());
 }
+
 
